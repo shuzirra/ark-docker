72d4a1c8c2785bfd2a66d099ec2f949fc3367c08
Gergely Pollak <shuzirra@cloudera.com>
2019-09-02 16:03:12 +0200

    Improving world save and backup timing
    Backup now waits for the world save to be clompleted unless it takes
    more than 60 seconds.

--- backup.sh
+++ backup.sh
@@ -4,6 +4,22 @@ echo "Backup started at "$START
 
 arkmanager broadcast "Server backup started ($START)"
 arkmanager saveworld
+
+timeout=0
+while [ $timeout -lt 60 ] && [ $(find /ark/server/ShooterGame/Saved/SavedArks -mmin -1 -name '*.ark' | wc -l) -lt 1 ]; do
+    sleep 1
+    timeout=$((timeout+1))
+    echo "Waiting save world to finish ("$timeout"s) ..."
+done
+
+if [ $timeout -ge 60 ]; then
+    arkmanager broadcast "World save timeout! Backing up latest save!"
+    echo "World save timeout! Backing up latest save!"
+else
+    arkmanager broadcast "World safe finished in "$timeout"s"
+    echo "World safe finished in "$timeout"s"
+fi
+
 arkmanager backup
 
 FINISH=`date +"%F_%T"`
